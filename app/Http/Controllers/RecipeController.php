38b75270f0d211429e361f1811d9f242a846f3d9
tommy <tommyevertsen52@gmail.com>
2023-09-19 17:23:22 +0200

    Made it possible to add recipes

---
+++ app/Http/Controllers/RecipeController.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Recipe;
+use Illuminate\Http\Request;
+use Inertia\Inertia;
+use Inertia\Response;
+use Illuminate\Http\RedirectResponse;
+
+class RecipeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(): Response
+    {
+        //
+        return Inertia::render('Recipe/Index', [
+            'recipes' => Recipe::with('user:id,name')->latest()->get()
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request): RedirectResponse
+    {
+        //
+        $validated = $request->validate([
+            'message' => 'required|string|max:255',
+        ]);
+
+        $request->user()->recipes()->create($validated);
+
+        return redirect(route('recipe.index'));
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Recipe $recipe)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Recipe $recipe)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Recipe $recipe)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Recipe $recipe)
+    {
+        //
+    }
+}
---
+++ resources/js/Pages/Recipe/Index.vue
@@ -0,0 +1,44 @@
+<template>
+    <Head title="Recipes" />
+
+    <AuthenticatedLayout>
+        <div class="max-w-2xl mx-auto p-4 sm:p-6 lg:p-8">
+            <form
+                @submit.prevent="
+                    form.post(route('recipe.store'), {
+                        onSuccess: () => form.reset(),
+                    })
+                "
+            >
+                <textarea
+                    v-model="form.message"
+                    placeholder="Please share you recipe with the world!"
+                    class="block w-full border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm"
+                ></textarea>
+                <InputError :message="form.errors.message" class="mt-2" />
+                <PrimaryButton class="mt-4">Add recipe</PrimaryButton>
+            </form>
+            <div class="mt-6 bg-white shadow-sm rounded-lg divide-y">
+                <Recipe
+                    v-for="recipe in recipes"
+                    :key="recipe.id"
+                    :recipe="recipe"
+                />
+            </div>
+        </div>
+    </AuthenticatedLayout>
+</template>
+
+<script setup>
+import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout.vue";
+import InputError from "@/Components/InputError.vue";
+import PrimaryButton from "@/Components/PrimaryButton.vue";
+import { useForm, Head } from "@inertiajs/vue3";
+import Recipe from "@/Components/Recipe.vue";
+
+const form = useForm({
+    message: "",
+});
+
+defineProps(["recipes"]);
+</script>
