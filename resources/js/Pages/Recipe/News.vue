db20f055896bef98dbe60f43f4a00b7ac2005fc0
Tommy Evertsen <94345410+TommyEvertsen@users.noreply.github.com>
2023-09-24 17:51:56 +0200

    Added news

---
+++ resources/js/Pages/Recipe/News.vue
@@ -0,0 +1,20 @@
+<template>
+<AuthenticatedLayout>
+
+    <div class="max-w-5xl mx-auto sm:px-6 lg:px-8 text-xl font-serif" >
+                <div class="bg-white overflow-hidden ">
+                    <div class="p-6 text-gray-900 text-center break-words">
+                       <p> Recipe share news"</p>
+                       <p>24.09-23 Added the most popular recipes feature</p>
+                       
+                    </div>
+                    
+                </div>
+            </div>
+</AuthenticatedLayout>
+</template>
+
+<script setup>
+import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
+
+</script>
