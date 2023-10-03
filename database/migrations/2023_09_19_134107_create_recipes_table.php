2f58460f234fa54044f5b7e0e995b897d113e485
tommy <tommyevertsen52@gmail.com>
2023-10-03 17:11:31 +0200

    new migrations

--- database/migrations/2023_09_19_134107_create_recipes_table.php
+++ database/migrations/2023_09_19_134107_create_recipes_table.php
@@ -13,9 +13,9 @@ return new class () extends Migration {
         Schema::create('recipes', function (Blueprint $table) {
             $table->id();
             $table->foreignId('user_id')->constrained()->cascadeOnDelete();
-            $table->text('message')->change();
+            $table->text('message');
             $table->string('title');
-            $table->text('ingridients')->change();
+            $table->text('ingridients');
             $table->integer('likes')->default(0);
             $table->timestamps();
         });
